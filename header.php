653032d8c962f094b5d5b88330c3692c55c638ae
ulmdesign <fbauhaus@gmail.com>
2024-05-10 11:10:49 +0200

    Update bootstrap version from cdn

--- header.php
+++ header.php
@@ -5,17 +5,16 @@
     <title>Todo list</title>
     <style>
     body{
-    padding:0;
-    margin:0;
-    background:url('background.jpg');
-    background-size:cover;
- 
-    background-repeat:no-repeat;
+      padding:0;
+      margin:0;
+      background:url('background.jpg');
+      background-size:cover;
+  
+      background-repeat:no-repeat;
     }
     </style>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
